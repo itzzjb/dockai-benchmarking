d1f86785e91f75cd619e468463281d38a64cc204
itzzjb <desilvabethmin@gmail.com>
2025-12-05 12:30:10 +0530

    Remove Node.js and Python Flask API implementations and related files; add repositories.json for project dependencies.

--- apps/nodejs-api/src/middleware/requestLogger.js
+++
@@ -1,8 +0,0 @@
-// Request logging middleware
-const requestLogger = (req, res, next) => {
-  const timestamp = new Date().toISOString();
-  console.log(`[${timestamp}] ${req.method} ${req.url}`);
-  next();
-};
-
-module.exports = requestLogger;
